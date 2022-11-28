19d5b5c34280432bc6988020c62853d48200fdb8
Daniel Abdel <danielmalek101@gmail.com>
2022-11-28 08:25:26 -0300

    Fix: textdecoration none Navibar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -29,6 +29,10 @@ const Container = styled.div`
   justify-content: space-between;
   align-items: center;
   box-shadow: rgba(17, 17, 26, 0.1) 0px 1px 0px;
+
+  a{ 
+    text-decoration: none;
+  }
 `;
 
 const Title = styled.div`
